e1725b7cd8985cf7c77c9109af9efbb1500b3ede
kaosay <58497583+kaosay@users.noreply.github.com>
2026-01-16 18:10:19 +0800

    Delete scripts directory

--- scripts/install-operator.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-kubectl apply -f CODES/golang/redis-operator-1/config/crd/bases/
-kubectl apply -f CODES/golang/redis-operator-1/config/manager/manager.yaml
-kubectl apply -f CODES/golang/redis-operator-1/config/rbac/serviceaccount.yaml
-kubectl apply -f CODES/golang/redis-operator-1/config/rbac/role.yaml
-kubectl apply -f CODES/golang/redis-operator-1/config/rbac/role_binding.yaml
